a1d61098e0112c91719f149e14f8fe436f976519
Patrick van Staveren <trick@vanstaveren.us>
2013-11-30 21:59:31 +0800

    Add example and remove commented code from generate.py

--- generate.py
+++ generate.py
@@ -11,25 +11,6 @@ for allocation in sys.argv[2].split(','):
     allocations.append(IPNetwork(allocation))
 output_file = sys.argv[3]
 
-"""network = IPNetwork('192.168.0.0/16')
-allocations = (
-    IPNetwork('192.168.1.0/24'),
-    IPNetwork('192.168.4.0/24'),
-    IPNetwork('192.168.16.0/23'),
-    IPNetwork('192.168.174.0/24'),
-    IPNetwork('192.168.199.0/26'),
-    IPNetwork('192.168.144.128/28'),
-    )
-
-network = IPNetwork('172.22.0.0/16') #  APAC Satellite offices allocation
-allocations = (
-    IPNetwork('172.22.0.0/20'), # KL
-    IPNetwork('172.16.16.0/20'), # SG
-    IPNetwork('172.16.32.0/20'), # BOM
-    IPNetwork('172.16.48.0/20'), # SYD
-    )
-"""
-
 width = 256
 height = network.size / width
 image = Image.new("RGB", (width, height), "white")
@@ -49,11 +30,5 @@ for ip in network:
     if column >= width:
         row += 1
         column = 0
-
-
-#for i in range(100):
-#    image.putpixel((i, i), red)
-
-
 
 image.save(output_file)
